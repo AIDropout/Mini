031df0af5015b053a736456d966b39f4fb71f1e4
avocadodip <56790819+avocadodip@users.noreply.github.com>
2024-07-20 18:25:38 -0700

    bird dev channel id local env only

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ async def configure_webhooks():
 
     _telegram = Telegram.from_config(config.MESSAGING_CONFIG.TELEGRAM)
     _bird = BirdSMSProvider.from_config(config.MESSAGING_CONFIG.BIRD)
-    _bird.set_channel_id(config.MESSAGING_CONFIG.BIRD.BIRD_DEV_CHANNEL_ID)
+    if hasattr(config.MESSAGING_CONFIG.BIRD, 'BIRD_DEV_CHANNEL_ID'):
+        _bird.set_channel_id(config.MESSAGING_CONFIG.BIRD.BIRD_DEV_CHANNEL_ID)
     webhook = f"{ngrok_connection.public_url}/message"
     await asyncio.gather(
         _telegram.register_webhook(webhook), 
