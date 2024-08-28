7eeac5e8ae091e565d3c7f8d9a23bd08f3baa214
avocadodip <56790819+avocadodip@users.noreply.github.com>
2024-08-28 16:37:04 -0700

    get_agents syntax error

--- zootopia/service/agent_service.py
+++ zootopia/service/agent_service.py
@@ -1,17 +1,18 @@
 from fastapi import HTTPException
 from zootopia.manager.database import DatabaseManager
 from zootopia.manager.payment.customer import CustomerManager
-from zootopia.core.schema.tables import Tables, User
+from zootopia.core.schema.tables import Tables, Agent
 from zootopia.service.base import Service
 from fastapi.responses import JSONResponse
 from postgrest.exceptions import APIError
+from typing import List
 
 
 class AgentService(Service):
     def __init__(self, database_manager: DatabaseManager):
         super().__init__(database_manager)
 
-    def get_agents(self) -> User:
+    def get_agents(self) -> List[Agent]:
         agents = self.database_manager.get_multiple_rows(
             Tables.AGENTS,
         )
