13bcbaee73d0a93bd2b1dacf4686bb536dca256a
avocadodip <56790819+avocadodip@users.noreply.github.com>
2024-08-30 16:24:59 -0700

    rename STRIPE_SECRET_KEY_TEST to STRIPE_SECRET_KEY

--- zootopia/manager/payment/stripe.py
+++ zootopia/manager/payment/stripe.py
@@ -8,6 +8,6 @@ class StripeBaseClient(ABC):
 
     def __init__(self):
         """Initializes the client with the Stripe API key."""
-        if not config.STRIPE_SECRET_KEY_TEST:
+        if not config.STRIPE_SECRET_KEY:
             raise ValueError("The Stripe API key must be set.")
-        stripe.api_key = config.STRIPE_SECRET_KEY_TEST
+        stripe.api_key = config.STRIPE_SECRET_KEY
